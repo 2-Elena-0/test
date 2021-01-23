1181a83543eeb762d0fae792073b28b67e8a9a39
mn.rumynin <mn.rumynin@e-soft.ru>
2021-01-23 18:55:26 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router/index.js'
+import router from './router'
 import dateFilter from "@/filters/date.filter";
 import messagePlugin from '@/utils/message.plugin'
-import store from './store/index.js'
+import store from './store'
 import 'materialize-css/dist/js/materialize.min'
 
 import firebase from 'firebase/app'
